cfd891691b15f6d4b26a0106cab9ffa38bfd955a
sablion <sabrinasuzana07@gmail.com>
2025-02-04 09:15:43 -0300

    Initial commit

---
+++ if.js
@@ -0,0 +1,57 @@
+// Atv. 1
+console.log("Atividade 1")
+let idade = prompt ("Qual a sua idade?:")
+if (idade >= 18){
+    console.log("Você é maior de idade")
+} else {
+    console.log("Você é menor de idade")
+}
+
+console.log("Atividade 2")
+let numero = prompt ("Escolha um número")
+let resultado = numero % 2
+if (resultado == 0){
+    console.log("O número é par")
+} else {
+    console.log("O número é impar")
+}
+
+console.log("Atividade 3")
+let nota = prompt("Qual a sua nota?:")
+if (nota >=7){
+    console.log("Aprovado")
+} else {
+    console.log("Reprovado")
+}
+
+console.log("Atividade 4")
+let SuaIdade = prompt ("Qual a sua idade?:")
+if (SuaIdade >= 18){
+    console.log("Entrada Permitida")
+} else {
+    console.log("Entrada proibida")
+}
+
+console.log("Atividade 5")
+let senha = prompt("Qual a sua senha?:")
+if (senha == 1234){
+    console.log("Senha correta")
+} else {
+    console.log("Senha incorreta")
+}
+
+console.log("Atividade 6")
+let temperatura = prompt ("Qual a temperatura?:")
+if (temperatura > 30){
+    console.log("Está quente")
+} else {
+    console.log("Está frio")
+}
+
+console.log ("Atividade 7")
+let Num = prompt ("Escolha um número:")
+if (Num >= 0){
+    console.log("Número positivo")
+} else {
+    console.log("Número negativo")
+}
